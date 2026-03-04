8658c9107eda0c9df0b03e9a12c1a7ff79b45636
Strikys12 <daqui.99@hotmail.com>
2026-03-03 21:31:05 -0500

    Ahora si, proyecto finalizado

--- src/main/java/com/grupo2/nexus/model/entity/Usuario.java
+++ src/main/java/com/grupo2/nexus/model/entity/Usuario.java
@@ -11,7 +11,7 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder // Añadido para mantener consistencia con Curso
-@ToString(exclude = "cursos") // Evitamos que imprima la lista de cursos
+@ToString
 
 public class Usuario {
     @Id
